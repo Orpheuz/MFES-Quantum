f07b6e6548ac7ebdc9bbe09e0fbeede0ba09d180
Vítor Teixeira <vitor_filipe_@hotmail.com>
2015-12-14 19:19:59 +0000

    cenas

--- generated/java/src/Quantum/Tower.java
+++ generated/java/src/Quantum/Tower.java
@@ -36,13 +36,7 @@ public class Tower extends Piece {
     }
 
     public void capturePiece(final Piece p) {
-        stack = SeqUtil.conc(p.getStack(), Utils.copy(stack));
-
-        if (Utils.equals(p.getOwner(), quantum.quotes.WhiteQuote.getInstance())) {
-            p.setOwner(quantum.quotes.BlackQuote.getInstance());
-        } else {
-            p.setOwner(quantum.quotes.WhiteQuote.getInstance());
-        }
+        stack = SeqUtil.conc(Utils.copy(stack), p.getStack());
     }
 
     public Number getSize() {
