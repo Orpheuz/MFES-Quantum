c1b31b26fdd0eac24bf9d3ff7925e6de8e3d38d9
Vítor Teixeira <vitor_filipe_@hotmail.com>
2015-12-14 09:12:47 +0000

    cenas

--- generated/java/src/Quantum/Main.java
+++ generated/java/src/Quantum/Main.java
@@ -1,22 +1,45 @@
 package quantum;
 
+import quantum.quotes.Player1Quote;
+
 public class Main {
 
 	public static void main(String[] args) {
 		Quantum game = new Quantum(false);
-		
-		for(int i = 1; i < Board.smallSizeY + 1 ; i++) {
-			for(int j = 1; j < Board.smallSizeX + 1 ; j++) {
+		printBoard();
+	}
+	
+	private static void printBoard() {
+		for(int i = 1; i < Board.smallSizeY + 1; i++) {
+			for(int j = 1; j < Board.smallSizeX + 1; j++) {
 				if(Board.getInstance().getTile(i, j).getPiece() != null)
-					System.out.print(" P ");
+					System.out.print(getPieceString(Board.getInstance().getTile(i, j).getPiece()));
 				else System.out.print
-				(" - ");
+				("| -- |");
 			}
 			System.out.println("");
 		}
 	}
 	
-	private String getPieceString(Piece p) {
+	private static String getPieceString(Piece p) {
+		if(p instanceof Circle) {
+			if(p.pieceOfPlayer.equals(Player1Quote.getInstance()))
+				return "| WO |";
+			else return "| BO |";
+		}
+		if(p instanceof Cross) {
+			if(p.pieceOfPlayer.equals(Player1Quote.getInstance()))
+				return "| W+ |";
+			else return "| B+ |";
+		}
+		if(p instanceof Square) {
+			if(p.pieceOfPlayer.equals(Player1Quote.getInstance()))
+				return "| WC |";
+			else return "| BC |";
+		}
+		if(p instanceof Tower) {
+			return "|T" + getPieceString(((Tower) p).getPieceTower());
+		}
 		return "";
 	}
 
